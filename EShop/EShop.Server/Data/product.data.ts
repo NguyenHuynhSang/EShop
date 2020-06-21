d9265117fa92a07def52de8018750053a44f1f27
NguyenHuynhSang <dev4fun.git@gmail.com>
2020-06-22 01:28:59 +0700

    seed from file Data/product.data.ts

---
+++ EShop/EShop.Server/Data/product.data.ts
@@ -0,0 +1,151 @@
+﻿[
+    {
+        "product": {
+            "id": 1,
+            "catalogID": 1,
+            "url": null,
+            "name": "Iphone test",
+            "description": "no discrip",
+            "content": "This is an iphone",
+            "weight": 0,
+            "originalPrice": 16000000,
+            "deliver": true,
+            "applyPromotion": true,
+            "createdDate": null,
+            "createdBy": null,
+            "modifiedDate": null,
+            "modifiedBy": null,
+            "seoTitle": null,
+            "seoUrl": null,
+            "seoDescription": null
+        },
+        "catalog": {
+            "id": 1,
+            "parentID": null,
+            "name": "Điện thoại",
+            "createdDate": null,
+            "createdBy": null,
+            "modifiedDate": null,
+            "modifiedBy": null,
+            "seoTitle": null,
+            "seoUrl": null,
+            "seoDescription": null
+        },
+        "productVersion": [
+            {
+                "id": 1,
+                "productID": 1,
+                "wareHouseID": 0,
+                "description": "Màu đỏ dl 250",
+                "price": 19000000,
+                "quantum": 100,
+                "remainingAmount": 100,
+                "sku": "Iphone test",
+                "barcode": "COC"
+            },
+            {
+                "id": 2,
+                "productID": 1,
+                "wareHouseID": 0,
+                "description": "Màu xanh dl 250",
+                "price": 18000000,
+                "quantum": 100,
+                "remainingAmount": 100,
+                "sku": "Iphone test",
+                "barcode": "COC"
+            }
+        ]
+    },
+    {
+        "product": {
+            "id": 2,
+            "catalogID": 1,
+            "url": null,
+            "name": "samsung galaxy test",
+            "description": "no discrip",
+            "content": "This is a samsung",
+            "weight": 0,
+            "originalPrice": 14000000,
+            "deliver": true,
+            "applyPromotion": true,
+            "createdDate": null,
+            "createdBy": null,
+            "modifiedDate": null,
+            "modifiedBy": null,
+            "seoTitle": null,
+            "seoUrl": null,
+            "seoDescription": null
+        },
+        "catalog": {
+            "id": 1,
+            "parentID": null,
+            "name": "Điện thoại",
+            "createdDate": null,
+            "createdBy": null,
+            "modifiedDate": null,
+            "modifiedBy": null,
+            "seoTitle": null,
+            "seoUrl": null,
+            "seoDescription": null
+        },
+        "productVersion": [
+            {
+                "id": 3,
+                "productID": 2,
+                "wareHouseID": 0,
+                "description": "Màu xanh dl 250",
+                "price": 16000000,
+                "quantum": 100,
+                "remainingAmount": 100,
+                "sku": "Iphone test",
+                "barcode": "COC"
+            }
+        ]
+    },
+    {
+        "product": {
+            "id": 4,
+            "catalogID": 2,
+            "url": "string",
+            "name": "Macbook Pro 2019",
+            "description": "Bản nâng cấp mới",
+            "content": "Bài giới thiệu",
+            "weight": 110,
+            "originalPrice": 40000000,
+            "deliver": true,
+            "applyPromotion": true,
+            "createdDate": "2020-06-11T21:23:03.9923668",
+            "createdBy": "string",
+            "modifiedDate": "2020-06-11T14:20:16.994",
+            "modifiedBy": "string",
+            "seoTitle": "string",
+            "seoUrl": "string",
+            "seoDescription": "string"
+        },
+        "catalog": {
+            "id": 2,
+            "parentID": null,
+            "name": "Laptop",
+            "createdDate": null,
+            "createdBy": null,
+            "modifiedDate": null,
+            "modifiedBy": null,
+            "seoTitle": null,
+            "seoUrl": null,
+            "seoDescription": null
+        },
+        "productVersion": [
+            {
+                "id": 4,
+                "productID": 4,
+                "wareHouseID": 0,
+                "description": "Bản cao cấp",
+                "price": 50000000,
+                "quantum": 100,
+                "remainingAmount": 200,
+                "sku": "MBB",
+                "barcode": "BAR"
+            }
+        ]
+    }
+]
