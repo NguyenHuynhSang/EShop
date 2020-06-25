df55ac455f5788fd72243561e0311d82707033d5
NguyenHuynhSang <dev4fun.git@gmail.com>
2020-06-25 20:31:58 +0700

    add product sorting option

--- EShop/EShop.Server/Controllers/ProductController.cs
+++ EShop/EShop.Server/Controllers/ProductController.cs
@@ -1,4 +1,4 @@
-﻿ using EShop.Server.Service;
+﻿using EShop.Server.Service;
 
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -10,25 +10,27 @@ using EShop.Server.FilterModel;
 using Newtonsoft.Json;
 using EShop.Server.Models;
 using EShop.Server.ViewModels;
+using EShop.Server.Extension;
+using System;
 
 namespace EShop.Server.Controllers
 {
-  
+
     [Route("api/[controller]/[action]")]
-   
+
     [ApiController]
-    public class ProductController:ControllerBase 
+    public class ProductController : ControllerBase
     {
         private IProductService _productService;// service xử dụng
 
         public ProductController(IProductService productService)
-            
+
         {
             this._productService = productService;
         }
 
         [HttpGet]
-        public ActionResult<PagedList<Product>> GetAllPaging(string productFilterModelJson, int pageNumder=1,int pageSize=50)
+        public ActionResult<PagedList<Product>> GetAllPaging(string productFilterModelJson, string sortBy, string sort= "desc", int pageNumder = 1, int pageSize = 50)
         {
             ProductFilterModel filterModel = null;
             if (!string.IsNullOrEmpty(productFilterModelJson))
@@ -36,21 +38,47 @@ namespace EShop.Server.Controllers
                 filterModel = JsonConvert.DeserializeObject<ProductFilterModel>(productFilterModelJson);
             }
 
-            var list = _productService.GetAll(filterModel) ;
+            var list = _productService.GetAll(filterModel);
+            switch (sort)
+            {
+                case "desc":
 
+                    list = sortBy == "name" ? list.Distinct().OrderByWithDirection(x => x.Name, true) : list;
+                    list = sortBy == "createdDate" ? list.Distinct().OrderByWithDirection(x => x.CreatedDate, true) : list;
+                    break;
+                case "asc":
+                    list = sortBy == "name" ? list.Distinct().OrderByWithDirection(x => x.Name, false) : list;
+                    list = sortBy == "createdDate" ? list.Distinct().OrderByWithDirection(x => x.CreatedDate, false) : list;
+                    break;
+                default:
+                    break;
+            }
             return PagedList<Product>.ToPagedList(list, pageNumder, pageSize);
         }
 
         [HttpGet]
-        public ActionResult<PagedList<ProductViewModel>> GetProductAllVersionsPaging(string productFilterModelJson, int pageNumder = 1, int pageSize = 50)
+        public ActionResult<PagedList<ProductViewModel>> GetProductAllVersionsPaging(string productFilterModelJson, string sortBy , string sort = "desc", int pageNumder = 1, int pageSize = 50)
         {
             ProductFilterModel filterModel = null;
+            var list = _productService.GetAllProductViewModel(filterModel);
             if (!string.IsNullOrEmpty(productFilterModelJson))
             {
                 filterModel = JsonConvert.DeserializeObject<ProductFilterModel>(productFilterModelJson);
             }
+            switch (sort)
+            {
+                case "desc":
 
-            var list = _productService.GetAllProductViewModel(filterModel);
+                    list = sortBy == "name" ? list.Distinct().OrderByWithDirection(x => x.Product.Name, true) : list;
+                    list = sortBy == "createdDate" ? list.Distinct().OrderByWithDirection(x => x.Product.CreatedDate, true) : list;
+                    break;
+                case "asc":
+                    list = sortBy == "name" ? list.Distinct().OrderByWithDirection(x => x.Product.Name, false) : list;
+                    list = sortBy == "createdDate" ? list.Distinct().OrderByWithDirection(x => x.Product.CreatedDate, false) : list;
+                    break;
+                default:
+                    break;
+            }
 
             return PagedList<ProductViewModel>.ToPagedList(list, pageNumder, pageSize);
         }
@@ -67,8 +95,8 @@ namespace EShop.Server.Controllers
         [HttpPost]
         public void CreateProductByProductInput(ProductInput product)
         {
-             _productService.CreateByProductInput(product);
-             
+            _productService.CreateByProductInput(product);
+
         }
 
         [HttpGet]
---
+++ EShop/EShop.Server/Extension/SortExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+public static class SortExtension
+{
+    public static IOrderedEnumerable<TSource> OrderByWithDirection<TSource, TKey>
+        (this IEnumerable<TSource> source,
+         Func<TSource, TKey> keySelector,
+         bool descending)
+    {
+        return descending ? source.OrderByDescending(keySelector)
+                          : source.OrderBy(keySelector);
+    }
+
+
+    public static IOrderedQueryable<TSource> OrderByWithDirection<TSource, TKey>
+        (this IQueryable<TSource> source,
+         Expression<Func<TSource, TKey>> keySelector,
+         bool descending)
+    {
+        return descending ? source.OrderByDescending(keySelector)
+                          : source.OrderBy(keySelector);
+    }
+
+
+}
