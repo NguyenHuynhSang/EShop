809dfeb7a3b9e390cf35d2843e085935d3a9dc47
NearHuscarl <near.huscarl@gmail.com>
2020-07-21 02:50:04 +0700

    cleanup

--- EShop/EShop.Web/src/app/widgets/Carousel.tsx
+++ EShop/EShop.Web/src/app/widgets/Carousel.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import MobileStepper from "@material-ui/core/MobileStepper";
 import Button from "@material-ui/core/Button";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import ArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
+import ArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import SwipeableViews from "react-swipeable-views";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
@@ -36,18 +36,11 @@ function SwipeableTextMobileStepper({
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const maxSteps = images.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step: number) => {
-    setActiveStep(step);
-  };
+  const handleNext = () => setActiveStep((step) => step + 1);
+  const handleBack = () => setActiveStep((step) => step - 1);
+  const handleStepChange = (step: number) => setActiveStep(step);
+  const isFirstStep = activeStep === 0;
+  const isLastStep = activeStep === maxSteps - 1;
 
   return (
     <div className={classes.root}>
@@ -70,18 +63,14 @@ function SwipeableTextMobileStepper({
         variant="dots"
         activeStep={activeStep}
         nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
+          <Button size="small" onClick={handleNext} disabled={isLastStep}>
             Next
-            <KeyboardArrowRight />
+            <ArrowRightIcon />
           </Button>
         }
         backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            <KeyboardArrowLeft />
+          <Button size="small" onClick={handleBack} disabled={isFirstStep}>
+            <ArrowLeftIcon />
             Back
           </Button>
         }
