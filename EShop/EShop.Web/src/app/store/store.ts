837a3e94a9977c29a187e42023a008f46f2ce5a2
NearHuscarl <near.huscarl@gmail.com>
2020-08-04 11:14:15 +0700

    add redux logger

--- EShop/EShop.Web/src/app/store/store.ts
+++ EShop/EShop.Web/src/app/store/store.ts
@@ -16,34 +16,51 @@ import {
   shallowEqual,
 } from 'react-redux';
 import { rootSaga, reducer } from './rootDuck';
+import { Middleware } from 'redux';
+
+let middlewares: Middleware[] = [];
+
+if (process.env.NODE_ENV === `development`) {
+  const { createLogger } = require(`redux-logger`);
+  const logger = createLogger({
+    collapsed: true,
+    duration: true,
+  });
+
+  middlewares.push(logger);
+}
 
 const sagaMiddleware = createSagaMiddleware();
-const middleware = [
-  ...getDefaultMiddleware({
-    thunk: false,
-    serializableCheck: {
-      // FIX: serialization issue when using redux-toolkit with redux-persist
-      // https://github.com/reduxjs/redux-toolkit/issues/121#issuecomment-611641781
-      ignoredActions: [
-        FLUSH,
-        REHYDRATE,
-        PAUSE,
-        PERSIST,
-        PURGE,
-        REGISTER,
-        'builder/setHtmlClassService',
-        // payload content is not serializable, but the final state after filtering is
-        'errors/setError',
-      ],
-      ignoredPaths: ['builder.htmlClassServiceObjects'],
-    },
-    immutableCheck: {
-      ignoredPaths: ['builder.htmlClassServiceObjects'],
-    },
-  }),
-  sagaMiddleware,
-];
-const store = configureStore({ reducer, middleware });
+const rtkMiddlewares = getDefaultMiddleware({
+  thunk: false,
+  serializableCheck: {
+    // FIX: serialization issue when using redux-toolkit with redux-persist
+    // https://github.com/reduxjs/redux-toolkit/issues/121#issuecomment-611641781
+    ignoredActions: [
+      FLUSH,
+      REHYDRATE,
+      PAUSE,
+      PERSIST,
+      PURGE,
+      REGISTER,
+      'builder/setHtmlClassService',
+      // payload content is not serializable, but the final state after filtering is
+      'errors/setError',
+    ],
+    ignoredPaths: ['builder.htmlClassServiceObjects'],
+  },
+  immutableCheck: {
+    ignoredPaths: ['builder.htmlClassServiceObjects'],
+  },
+});
+
+middlewares = middlewares.concat(rtkMiddlewares);
+middlewares.push(sagaMiddleware);
+
+const store = configureStore({
+  reducer,
+  middleware: middlewares,
+});
 
 /**
  * @see https://github.com/rt2zz/redux-persist#persiststorestore-config-callback
