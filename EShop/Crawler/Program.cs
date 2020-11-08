086f7137cb924afe5ad9e0d46412e3949082f139
NguyenHuynhSang <dev4fun.git@gmail.com>
2020-11-08 22:12:51 +0700

    crawl api web

---
+++ EShop/Crawler/Program.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    public class Program
+    {
+        public static string base_curl_path = @"../../../Curl/vietnamwork_it_en.curl";
+        public static HttpClient httpClient;
+        public static RestClient restClient;
+        public static IWebDriver webDriver;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CrawlDataFromApi(base_curl_path);
+        }
+
+
+
+        public static string CrawlDataFromUrl(string url)
+        {
+
+            var htmlReturn = "";
+            htmlReturn = httpClient.GetStringAsync(url).Result;
+            return htmlReturn;
+        }
+
+
+        public void Crawl(string url)
+        {
+
+
+
+        }
+
+
+        public static void CrawlDataFromApi(string path)
+        {
+            var cUrl = File.OpenText(path).ReadToEnd();
+            restClient = new RestClient(cUrl);
+       
+            List<string> headers = new List<string>();
+            string url= Regex.Match(cUrl, @"(?<=curl ')(.*?)(?=' \\)", RegexOptions.Singleline).Value;
+            var headerList= Regex.Matches(cUrl, @"(?<=-H ')(.*?)(?=' \\)", RegexOptions.Singleline).Select(x=>x.Value.ToString());
+            foreach (var item in headerList)
+            {
+                restClient.AddDefaultHeader("Header",item);
+
+            }
+           
+
+
+
+        }
+
+
+
+
+    }
+}
