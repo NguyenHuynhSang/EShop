3216a0640a0729c54868dc19ed994142f8299f39
NguyenHuynhSang <dev4fun.git@gmail.com>
2020-12-22 11:52:12 +0700

    cleanup product catalog menu

--- EShop/EShop.Server/Client/Dtos/ProductCatalogForMenuDto.cs
+++ EShop/EShop.Server/Client/Dtos/ProductCatalogForMenuDto.cs
@@ -9,14 +9,14 @@ namespace EShop.Server.Client.Dtos
     {
         public int Id { set; get; }
 
-        public int? ParentID { set; get; }
+       // public int? ParentID { set; get; }
 
         public string Name { set; get; }
 
         public string Image { set; get; }
-        public string SEOTitle { get; set; }
-        public string SEOUrl { get; set; }
-        public string SEODescription { get; set; }
+        //public string SEOTitle { get; set; }
+        //public string SEOUrl { get; set; }
+        //public string SEODescription { get; set; }
         public List<ProductCatalogForMenuDto> ChildCatalogs { set; get; }
 
 
